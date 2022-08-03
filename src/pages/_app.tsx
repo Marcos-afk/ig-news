4298ef7d7b91c39a5036695499cd70c36abc1677
Marcos André <91892865+DevTeamSPA@users.noreply.github.com>
2022-08-03 00:59:01 -0300

    implementation of authentication with Next Auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Header } from '../components/Header';
+import { SessionProvider } from 'next-auth/react';
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
