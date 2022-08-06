4cb5fa4e0a144436b75a3a227117188b52d64a2a
Marcos André <72817154+Marcos-afk@users.noreply.github.com>
2022-08-06 07:29:58 -0300

    subscription function implementation

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,11 +1,31 @@
+import { signIn, useSession } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 import { SubscribeButtonProps } from './types/SubscribeButtonProps';
 
+// eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
-  // eslint-disable-next-line no-console
-  console.log(priceId);
+  const { data: Session } = useSession();
+  const handleSubscribe = async () => {
+    if (!Session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const { data } = await api.post('/subscribe');
+      const { sessionId } = data;
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button type="button" className={styles.subscribeButton} onClick={handleSubscribe}>
       Subscribe now
     </button>
   );
